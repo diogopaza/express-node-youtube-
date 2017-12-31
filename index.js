04a26e7459c82f0debf379987ac3cfb1c5d2421c
Diogo Paza <diogopazacvel@gmail.com>
2017-12-31 15:39:19 -0200

    finalizando

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const express =  require('express')
-
+const express =  require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
 
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const pessoas =[
+
+    {   
+        email:'maria@gmail.com',
+        password:'1234'
+    },
+    {
+        email:'julio@gmail.com',
+        password:'4321'
+
+    },
+]
+
 app.get('/', function(request, response){
+    
+        response.write('<h1>olá</h1>')
+        response.end();
+    
+    });
 
-    response.send('Hello mais um ')
 
+app.get('/pessoas', function(request, response){
+
+    response.json(pessoas)
+    pessoas.push(
+        {
+            email: "maisuma@mmm",
+            password: "7897987"
+        }
+    )
+
+});
+
+app.post('/pessoas', function(request, response){
+    //response.send('resposta do post2222')
+    console.log(request.body);
+    
+    pessoas.push(
+        {
+            email: request.body.email,
+            password: request.body.password
+        }
+    )
+    
+    response.end()
 })
+
 
 app.listen(3000);
 
